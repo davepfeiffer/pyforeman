2888be3132428e8fa822918b8d17703bdb05b60f
David Pfeiffer <davepfeifferer@gmail.com>
2017-09-01 19:05:18 -0400

    polish up script format and temp add a hw builder

--- foreman/foreman.py
+++ foreman/foreman.py
@@ -10,6 +10,6 @@ def construct_foundation(foundation, env):
   return env.union(new_materials)
 
 # Blueprint -> Material Set -> Material Set
-def construct(blueprint, env):
+def construct(blueprint, env=frozenset()):
   env_p = construct_foundation(blueprint.foundation, env)
   return blueprint.builder(env_p, blueprint.name, blueprint.materials)
